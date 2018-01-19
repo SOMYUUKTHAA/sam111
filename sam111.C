b6061f95a491479c62923679a01ed280ec397117
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-01-19 10:26:59 +0000

    Create sam111.C

---
+++ sam111.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("\n ENTER A VALUE");
+    scanf("%d",&n);
+    while(n/10==0)
+    {
+        a++;
+    }
+    }
+    printf("\nTHE NUMBER OF DIGITS ENTERED ARE:\t%d",a);
+}
